2db8bdd83e642c26f987635d64befe250233f45c
kishore-devaraj <kishoregrylls@gmail.com>
2018-10-01 02:07:30 +0530

    Muliple scrum added for UI testing and project side view added

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,10 @@
 
 window.addEventListener('load', () => {
     const app = document.getElementById('app')
-    const homeTemplate = document.getElementById('main-page').innerHTML
-    
-
-
-    app.innerHTML = homeTemplate
+    const homeTemplate = Handlebars.compile(document.getElementById('main-page').innerHTML)
+    const scrumMinCard = document.getElementById('scrum-min').innerHTML
+    const homeTemplateHtml = homeTemplate({
+        'scrum': [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    })
+    app.innerHTML = homeTemplateHtml
 })
